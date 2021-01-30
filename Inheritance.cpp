30feb2c1bf8a9fb7e535f8ad358dac9b54d87ae9
Kundro <exeykundro@gmail.com>
2021-01-30 13:58:47 +0300

    Added constructor

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -6,14 +6,26 @@ protected:
 	int i, j;
 
 public:
+	base() { i = 0; j = 0; cout << "Конструктор base." << endl; }
+	~base() { cout << "Деструктор base." << endl; }
 	void set(int a, int b) { i = a; j = b; }
 	void show() { cout << i << ' ' << j << '\n'; }
 };
 
-class derived : public base {
+class base2 {
+protected:
+	int a, b;
+public:
+	base2() { a = 0; b = 0; cout << "Конструктор base2." << endl;}
+	~base2() { cout << "Деструктор base2." << endl; }
+	void set2(int a, int b) { this->a = a; this->b = b; }
+	void show2() { cout << a << ' ' << b << '\n'; }
+};
+
+class derived : public base, public base2 {
 	int k;
 public:
-	void setk() { k = i * j; }
+	void setk() { k = i + j + a + b; }
 	void showk() { cout << k << '\n'; }
 };
 
@@ -27,4 +39,4 @@ int main()
 	
 
 	return 0;
-}
+  }
